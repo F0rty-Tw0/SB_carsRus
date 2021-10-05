d6e07cdb4d13fd8be38610158b846323d0a16994
Art <artiomtofancv@gmail.com>
2021-10-05 13:01:46 +0200

    created a logic to not create database all the times

--- src/main/java/cars/rus/Configuration/DataSetup.java
+++ src/main/java/cars/rus/Configuration/DataSetup.java
@@ -17,38 +17,42 @@ import cars.rus.Repositories.ReservationRepository;
 @Component
 @Profile("!test")
 public class DataSetup implements CommandLineRunner {
-  private CarRepository carRepository;
-  private MemberRepository memberRepository;
-  private ReservationRepository reservationRepository;
+    private CarRepository carRepository;
+    private MemberRepository memberRepository;
+    private ReservationRepository reservationRepository;
 
-  public DataSetup(CarRepository carRepository, MemberRepository memberRepository,
-      ReservationRepository reservationRepository) {
-    this.carRepository = carRepository;
-    this.memberRepository = memberRepository;
-    this.reservationRepository = reservationRepository;
-  }
+    public DataSetup(CarRepository carRepository, MemberRepository memberRepository,
+            ReservationRepository reservationRepository) {
+        this.carRepository = carRepository;
+        this.memberRepository = memberRepository;
+        this.reservationRepository = reservationRepository;
+    }
 
-  @Override
-  public void run(String... args) throws Exception {
-    carRepository.save(new Car("Audi", "A5", 60));
-    carRepository.save(new Car("Toyota", "Corolla", 30));
-    carRepository.save(new Car("Toyota", "Yaris", 30));
-    carRepository.save(new Car("Mercedes", "CLA", 50));
-    carRepository.save(new Car("Porsche", "Cayenne", 100));
+    @Override
+    public void run(String... args) throws Exception {
+        if (carRepository.findAll().size() == 0 && memberRepository.findAll().size() == 0
+                && reservationRepository.findAll().size() == 0) {
+            carRepository.save(new Car("Audi", "A5", 60));
+            carRepository.save(new Car("Toyota", "Corolla", 30));
+            carRepository.save(new Car("Toyota", "Yaris", 30));
+            carRepository.save(new Car("Mercedes", "CLA", 50));
+            carRepository.save(new Car("Porsche", "Cayenne", 100));
 
-    memberRepository
-        .save(new Member("Artiom", "Tofan", "30 Commercial Road", "New York", "1526", "art@gmail.com", true, 5));
-    memberRepository.save(new Member("John", "Digweed", "Piedras 623", "Brussels", "2010", "john@gmail.com", true, 5));
-    memberRepository
-        .save(new Member("Paul", "Van Dyke", "French 392", "Manpack", "IX991", "Ppaul@gmail.com", false, 6));
-    memberRepository.save(new Member("Armin", "Van Buuren", "219–241 Cleveland St", "Chesterfield", "10991",
-        "armin@gmail.com", false, 3));
-    memberRepository
-        .save(new Member("David", "Guetta", "Hauptstr. 5", "Buenas Aires", "5818", "david@gmail.com", false, 2));
+            memberRepository.save(
+                    new Member("Artiom", "Tofan", "30 Commercial Road", "New York", "1526", "art@gmail.com", true, 5));
+            memberRepository
+                    .save(new Member("John", "Digweed", "Piedras 623", "Brussels", "2010", "john@gmail.com", true, 5));
+            memberRepository.save(
+                    new Member("Paul", "Van Dyke", "French 392", "Manpack", "IX991", "Ppaul@gmail.com", false, 6));
+            memberRepository.save(new Member("Armin", "Van Buuren", "219–241 Cleveland St", "Chesterfield", "10991",
+                    "armin@gmail.com", false, 3));
+            memberRepository.save(
+                    new Member("David", "Guetta", "Hauptstr. 5", "Buenas Aires", "5818", "david@gmail.com", false, 2));
 
-    reservationRepository.save(new Reservation(LocalDate.of(2021, Month.JANUARY, 24),
-        memberRepository.findById(1l).get(), carRepository.findById(1l).get()));
-    reservationRepository.save(new Reservation(LocalDate.of(2021, Month.JANUARY, 25),
-        memberRepository.findById(2l).get(), carRepository.findById(1l).get()));
-  }
+            reservationRepository.save(new Reservation(LocalDate.of(2021, Month.JANUARY, 24),
+                    memberRepository.findById(1l).get(), carRepository.findById(1l).get()));
+            reservationRepository.save(new Reservation(LocalDate.of(2021, Month.JANUARY, 25),
+                    memberRepository.findById(2l).get(), carRepository.findById(1l).get()));
+        }
+    }
 }
--- src/main/java/cars/rus/Controller/HomeController.java
+++ src/main/java/cars/rus/Controller/HomeController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 @RestController
 public class HomeController {
+  @ApiIgnore
   @GetMapping("/")
   public RedirectView  index() {
     return new RedirectView("swagger-ui/index.html");
